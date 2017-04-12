bc0ce22d192a5d9767c2aef91c3032d28403a72d
anthony <anthony@132.238.233.137>
2017-04-12 19:29:26 -0400

    initial comment

---
+++ src/com/mycompany/expgit2/chapter1/myclass.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package com.mycompany.expgit2.chapter1;
+
+/**
+ * @author anthony
+ *
+ */
+public class myclass {
+	private String name;
+	public String getName() {
+		return name;
+	}
+	private String lastname;
+	public myclass(String name){
+		this.name = name;
+	}
+		// TODO Auto-generated constructor stub
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("BEGIN");
+		staticMethod("ilker");
+		System.out.println();("END");
+
+	}
+	public static void staticMethod(String inp){
+		System.out.println("Hi input is " + inp);
+		System.out.println("bye");
+	}
+	
+
+}
